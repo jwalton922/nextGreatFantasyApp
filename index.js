884ef2b9d24bc2af55c8b75384f88a4046b7ffd1
Joshua Walton <jwalton@Joshuas-MacBook-Pro.local>
2015-09-25 15:05:34 -0600

    mess with renew token

--- index.js
+++ index.js
@@ -416,12 +416,12 @@ function renewYahooTokens(userObj){
             "&oauth_timestamp="+oauth_timestamp+
             "&oauth_session_handle="+oauth_session_handle;
     console.log("renew token url: "+urlWithQureryParams);
-    var response = rp.get(urlWithQureryParams).then(function(body){
+    var renewResponse = rp.get(urlWithQureryParams).then(function(body){
         console.log("Token renew response: "+JSON.stringify(body));
         return body;
     });
     
-    return response;
+    return renewResponse;
 }
 
 app.get('/renewYahooToken', function(request, response){
